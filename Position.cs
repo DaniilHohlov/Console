a208f5a470dfa9af018f5b6b0324b3738b153b79
DaniilHohlov <daniilhohlov5@gmail.com>
2024-12-06 19:48:43 +0300

    Убрал цикл While. cоздал новый класс Position в который перенес создание сделки и при помощи Trade вывожу информацию по ней.

---
+++ Position.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Timers;
+
+
+namespace MyConsole
+{
+    public class Position
+    {
+        public Position()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 5000;              // каждые 7000 мс будет производится действие
+            Console.WriteLine("Ждем " + timer.Interval / 1000 + " сек.");
+            timer.Elapsed += NewTrade;          // вызов действия
+
+            timer.Start();                      // запуск таймера в параллельном потоке
+        }
+        public void NewTrade(object sender, ElapsedEventArgs e)
+        {
+            Random random = new Random();
+
+            string[] CurrencyPair = { "EUR", "USD" };
+
+            string RandomPair = CurrencyPair[random.Next(0, CurrencyPair.Length)];
+
+            int num = random.Next(-10, 10);
+            TradeDirection direction;
+            int newPrice = random.Next(70000, 80000);
+            int ExitPrice = random.Next(70000, 80000);
+            decimal StopLoss = newPrice;
+            if (newPrice > ExitPrice)
+            {
+                StopLoss += (StopLoss * 6 / 100);
+                direction = TradeDirection.Short;
+            }
+            else
+            {
+                StopLoss -= (StopLoss * 6 / 100);
+                direction = TradeDirection.Long;
+            }
+
+
+            TradeManager manager = new TradeManager();
+            Trade trade = new Trade(direction, RandomPair, Math.Abs(num), newPrice, random.Next(1, 5), ExitPrice, StopLoss);
+
+            Console.WriteLine($"Новая сделка:");
+            Console.WriteLine($"Направление: {trade.Direction}");
+            Console.WriteLine($"Валюта: {trade.Pair}");
+            Console.WriteLine($"Объем: {trade.Volume}");
+            Console.WriteLine($"Цена входа: {trade.Price}");
+            Console.WriteLine($"Количество открытых лотов: {trade.OpenLotsCount}");
+            Console.WriteLine($"Цена выхода: {trade.TakeProfit}");
+            Console.WriteLine($"Стоп Лосс: {trade.StopLoss}");
+            Console.WriteLine($"Время создания: {trade.CreationTime}");
+
+            manager.WriteTradeToFile(trade);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,92 +1,21 @@
 ﻿using System;
-using System.Threading;
-using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace MyConsole
 {
 
-    
-
-public class Program
-    {
-        static void Main(string[] args)
+    public class Program
         {
-            while (true)
+            static void Main(string[] args)
             {
-                Random random = new Random();
-
-                string[] CurrencyPair = {"EUR", "USD"};
-
-                string RandomPair = CurrencyPair[random.Next(0, CurrencyPair.Length)];
-
-                int num = random.Next(-10, 10);
-                TradeDirection direction;
-                int newPrice = random.Next(70000, 80000);
-                int ExitPrice = random.Next(70000, 80000);
-                if (newPrice > ExitPrice)
-                {
-                    direction = TradeDirection.Short;
-                }
-                else
-                {
-                    direction = TradeDirection.Long;
-                }
-
-                
-                TradeManager manager = new TradeManager();
-                Trade trade = new Trade(direction, RandomPair, Math.Abs(num), newPrice, random.Next(1, 5), ExitPrice, StopLoss);
-
-                Console.WriteLine($"Новая сделка:");
-                Console.WriteLine($"Направление: {trade.Direction}");
-                Console.WriteLine($"Валюта: {trade.Pair}");
-                Console.WriteLine($"Объем: {trade.Volume}");
-                Console.WriteLine($"Цена входа: {trade.Price}");
-                Console.WriteLine($"Количество открытых лотов: {trade.OpenLotsCount}");
-                Console.WriteLine($"Цена выхода: {trade.TakeProfit}");
-                Console.WriteLine($"Стоп Лосс: {trade.StopLoss}");
-                Console.WriteLine($"Время создания: {trade.CreationTime}");
-
-                manager.WriteTradeToFile(trade);
-
-                Thread.Sleep(5000);
-            }
+                Position position = new Position();
             
-
+                Console.ReadLine();
+            }
         }
-    }
-
-
-
-    //static void Main(string[] args)
-    //{
-
-    //Position position = new Position();
-
-
-
-    //    //Console.Write("Задайте верхнюю цену: ");
-    //    //double upPrice = double.Parse(Console.ReadLine());
-
-    //    //Console.Write("Задайте нижнюю цену: ");
-    //    //double downPrice = double.Parse(Console.ReadLine());
-
-    //    //Console.Write("Задайте шаг уровня: ");
-    //    //double step = double.Parse(Console.ReadLine());
-    //    //int Count = 0;
-
-
-    //    //while ((upPrice - step) >= downPrice)
-    //    //{
-
-    //    //    Count++;
-    //    //    upPrice -= step;
-    //    //    Console.WriteLine("{0:f1}", upPrice);
-    //    //}
-    //    //Console.WriteLine($"Колличество уровней: "+ Count);
-
-
-    //}
-
-
 }
 
         
